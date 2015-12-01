91514dfc2a83a446fba497adf4c62aa674f9c61c
Rodrigo Ferreira <r.caneppele@gmail.com>
2015-12-01 12:52:14 -0200

    Front-Controller do JAX-RS

---
+++ src/br/com/rcaneppele/blog/infra/RestConfig.java
@@ -0,0 +1,9 @@
+package br.com.rcaneppele.blog.infra;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class RestConfig extends Application {
+
+}
