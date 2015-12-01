f69f35f3e4b34f2b77c3db2d20c9a7f692953525
Rodrigo Ferreira <r.caneppele@gmail.com>
2015-12-01 11:53:04 -0200

    Home Servlet

---
+++ WebContent/home.jsp
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="pt">
+<head>
+	<meta charset="UTF-8" />
+	<title>Mongo Blog</title>
+</head>
+
+<body>
+	<h1>WELCOME!</h1>
+</body>
+</html>
---
+++ src/br/com/rcaneppele/blog/servlet/HomeServlet.java
@@ -0,0 +1,21 @@
+package br.com.rcaneppele.blog.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		req.getRequestDispatcher("home.jsp").forward(req, res);
+	}
+	
+}
